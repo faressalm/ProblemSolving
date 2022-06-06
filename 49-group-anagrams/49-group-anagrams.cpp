ec25bd1121bc84b87d290bc19fd688fc0c619881
fares waheed <faressalm44@hotmail.com>
2022-06-06 18:42:00 +0200

    Time: 46 ms (64.44%), Space: 21.8 MB (21.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -6,23 +6,21 @@ public:
         vector<string> s = strs;
         for(int i=0;i<s.size();i++){
            sort(s[i].begin(),s[i].end());
+           anagrams[s[i]].push_back(i);
         }
-        for(int i=0;i<s.size();i++){
-            anagrams[s[i]].push_back(i);
-        }
-    unordered_map<string,vector<int>>::iterator it = anagrams.begin();
+        unordered_map<string,vector<int>>::iterator it = anagrams.begin();
      
-    // Iterating over the map using Iterator till map end.
-    while (it != anagrams.end())
-    {
-        // Accessing the value
-        vector<int> ana = it->second;
-        vector<string> group; 
-        for(auto i:ana)
-            group.push_back(strs[i]);
-        sol.push_back(group);
-        it++;
-    }
+        // Iterating over the map using Iterator till map end.
+        while (it != anagrams.end())
+        {
+            // Accessing the value
+            vector<int> ana = it->second;
+            vector<string> group; 
+            for(auto i:ana)
+                group.push_back(strs[i]);
+            sol.push_back(group);
+            it++;
+        }
         return sol;
     }
 };
