23e26debf89643081649b8804bdd6f08c7e3778c
fares waheed <faressalm44@hotmail.com>
2022-06-06 18:40:55 +0200

    Time: 66 ms (31.03%), Space: 21.8 MB (20.50%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<int>> anagrams;
+        vector<vector<string>>  sol;
+        vector<string> s = strs;
+        for(int i=0;i<s.size();i++){
+           sort(s[i].begin(),s[i].end());
+        }
+        for(int i=0;i<s.size();i++){
+            anagrams[s[i]].push_back(i);
+        }
+    unordered_map<string,vector<int>>::iterator it = anagrams.begin();
+     
+    // Iterating over the map using Iterator till map end.
+    while (it != anagrams.end())
+    {
+        // Accessing the value
+        vector<int> ana = it->second;
+        vector<string> group; 
+        for(auto i:ana)
+            group.push_back(strs[i]);
+        sol.push_back(group);
+        it++;
+    }
+        return sol;
+    }
+};
