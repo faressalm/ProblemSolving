696164c0da023b91ba0218875d80e8d76bfa7a2c
fares waheed <faressalm44@hotmail.com>
2022-06-07 06:47:05 +0200

    Time: 8 ms (9.71%), Space: 8.4 MB (75.38%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,27 +1,16 @@
 class Solution {
-public:
-    bool check(vector<int>& nums) {
-        if(nums.size()<2)
-            return true;
-        bool flipped =false;
-        for(int i=1;i<nums.size()-1;i++){
-            cout<<i;
-            if(nums[i-1]>nums[i])
-            {   
-                if(flipped)
-                    return false;
-                else
-                    flipped = true;                
-            }
-
-            if(nums[i-1]<nums[i+1]&&nums[i]<nums[i-1])
-                return false;
+  public:
+    bool check(vector<int> & nums) {
+      int count = 0;
+      int n = nums.size();
+      for (int i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+          count = count + 1;
         }
-        if(nums[nums.size()-2]>nums[nums.size()-1])
-            if(flipped) return false;
-            else flipped =true;
-        if(flipped&&(nums[nums.size()-1]>nums[0]) )
-            return false;
+      }
+      if (count <= 1) {
         return true;
+      } else
+        return false;
     }
 };
