6232c43d05d711670f9ac768b345721bab985081
fares waheed <faressalm44@hotmail.com>
2022-06-09 05:02:43 +0200

    Time: 24 ms (26.00%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<pair<bool,int>> sums(2004,{false,-1});
-        for(int i=0;i<numbers.size();i++)
-            sums[numbers[i]+1000] = {true,i};
-        for(int i=0;i<numbers.size();i++){
-            int next = target - numbers[i]+1000;
-            if(next<2004&&sums[next].first&&sums[next].second!=i)
-                return {i+1,sums[next].second+1};
+        int start=0;
+        int end=numbers.size()-1;
+        while(start<end){
+          if(numbers[start]+numbers[end] == target)
+              return {start+1,end+1};
+          if(numbers[start]+numbers[end]<target)
+              start++;
+          else
+              end--;
         }
         return {};
     }
