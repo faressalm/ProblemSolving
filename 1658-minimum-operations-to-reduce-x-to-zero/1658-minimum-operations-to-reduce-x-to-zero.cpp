0a331af4528b0c0903d570cafa338586c46ee8b6
fares waheed <faressalm44@hotmail.com>
2022-06-11 07:52:16 +0200

    Time: 272 ms (60.22%), Space: 98.5 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        unordered_map<int,int> perfix;
-        perfix[nums[0]]=0;
+        if(nums.size()==1)
+            if(nums[0]==x)
+                return 1;
+            else return -1;
+        int sum =0;
+        for(auto num:nums)
+            sum+=num;
+        if(x==sum)
+            return nums.size();
         int brev= nums[0];
-        for(int i=1;i<nums.size();i++){
-            brev+=nums[i];
-            perfix[brev]=i;
-        }
-        brev = 0;
+        int preBrev = 0;
+        int start=0;
         int minIndex = INT_MAX;
-        if(perfix.find(x)!=perfix.end())
-            minIndex = perfix[x]+1;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-          brev += nums[i];  
-          if(brev>x)
-              break;
-          if(brev==x&&(nums.size()-i)<=minIndex)
-              return nums.size() - i;
-          if(perfix.find(x - brev)!=perfix.end()) 
-          {
-              int rightIndex = perfix[x - brev];
-              if(rightIndex<i){
-                  minIndex =  min(minIndex,(int)nums.size()-i+rightIndex+1);
-              }
-          } 
+        int end=1;
+        while(start<nums.size()&&end<=nums.size()&&start<=end){
+            if((brev-preBrev)==(sum-x)){
+                minIndex = min(minIndex,(int)nums.size()-(end-start)); 
+                end++;
+                brev += nums[end-1];                 
+            }
+            else if( brev-preBrev < sum-x){
+                end++;
+                brev += nums[end-1]; 
+            }else{
+                preBrev+=nums[start];
+                start++;
+            }
         }
         if(minIndex!=INT_MAX)
             return minIndex;
