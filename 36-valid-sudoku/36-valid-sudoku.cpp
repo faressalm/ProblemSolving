75ff50f18191a61dd7fab5d5a20a90d02840c217
fares waheed <faressalm44@hotmail.com>
2022-06-12 21:33:13 +0200

    Time: 15 ms (97.86%), Space: 18.1 MB (74.89%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,19 +3,19 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n= board.size();
         for(int i=0;i<n;i++){
-            vector<bool> rowS(n+1,false);
-            vector<bool> colS(n+1,false);
+            int rowS=0;
+            int colS=0;
             for(int j=0;j<n;j++){
                 if(board[i][j]!='.')
-                if(rowS[board[i][j]-'0'])
+                if((rowS&(1<<(board[i][j]-'0') ))!=0)
                     return false;
                 else
-                    rowS[board[i][j]-'0'] =true;
+                    rowS=rowS| (1<<(board[i][j]-'0'));
                 if(board[j][i]!='.')
-                if(colS[board[j][i]-'0'])
+                if((colS&(1<<(board[j][i]-'0')) )!= 0)
                     return false;
                 else
-                    colS[board[j][i]-'0'] =true;
+                    colS =colS|(1<<(board[j][i]-'0'));
             }
         }
       for(int i=0;i<n;i+=3){
