3cd8909e1a5bd5ec8f2b719bfd689207b217118c
fares waheed <faressalm44@hotmail.com>
2022-06-12 21:46:07 +0200

    Time: 33 ms (51.68%), Space: 18 MB (74.64%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n= board.size();
+        vector<int> boxs(n+1,0);
         for(int i=0;i<n;i++){
             int rowS=0;
             int colS=0;
             for(int j=0;j<n;j++){
+                int boxIndex = (i/3)*3 + j/3;
                 if(board[i][j]!='.')
-                if((rowS&(1<<(board[i][j]-'0') ))!=0)
+                {
+                 if((rowS&(1<<(board[i][j]-'0') ))!=0)
                     return false;
-                else
-                    rowS=rowS| (1<<(board[i][j]-'0'));
+                 else
+                    rowS=rowS| (1<<(board[i][j]-'0'));  
+                 if((boxs[boxIndex]&(1<<(board[i][j]-'0') ))!=0)
+                    return false;
+                 else
+                    boxs[boxIndex]=boxs[boxIndex]|(1<<(board[i][j]-'0'));   
+                }
+
                 if(board[j][i]!='.')
                 if((colS&(1<<(board[j][i]-'0')) )!= 0)
                     return false;
                 else
                     colS =colS|(1<<(board[j][i]-'0'));
-            }
-        }
-      for(int i=0;i<n;i+=3){
-            int k=i;
-            for(int j=0;j<n;j+=3){
-              int boxS = 0;
-               for(int x=j;x<j+3;x++) 
-                for(int k=i;k<i+3;k++){
-                    if(board[k][x]!='.')
-                    if((boxS&(1<<(board[k][x]-'0') ))!=0)
-                      return false;
-                    else
-                      boxS =boxS|(1<<(board[k][x]-'0'));
-                }
+                
             }
         }
         return true;
