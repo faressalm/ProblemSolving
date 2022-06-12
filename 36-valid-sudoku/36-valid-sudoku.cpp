c755ce45e4200c5252d64afe7128a7dfe2a65096
fares waheed <faressalm44@hotmail.com>
2022-06-12 21:37:20 +0200

    Time: 52 ms (13.26%), Space: 17.9 MB (96.91%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -21,14 +21,14 @@ public:
       for(int i=0;i<n;i+=3){
             int k=i;
             for(int j=0;j<n;j+=3){
-              vector<bool> boxS(n+1,false);
+              int boxS = 0;
                for(int x=j;x<j+3;x++) 
                 for(int k=i;k<i+3;k++){
                     if(board[k][x]!='.')
-                    if(boxS[board[k][x]-'0'])
+                    if((boxS&(1<<(board[k][x]-'0') ))!=0)
                       return false;
                     else
-                      boxS[board[k][x]-'0'] =true;
+                      boxS =boxS|(1<<(board[k][x]-'0'));
                 }
             }
         }
