fc065e0409a6db2636597c3bed82bbe133a38da8
fares waheed <faressalm44@hotmail.com>
2022-06-19 07:35:51 +0200

    Time: 281 ms (32.01%), Space: 76.4 MB (46.89%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,71 +1,73 @@
-class Solution {
-private:
-// trie node
-struct TrieNode
+// Custom class Trie with function to get 3 words starting with given prefix
+class Trie
 {
-    struct TrieNode *children[26];
-    bool isEndOfWord;
-};
-// Returns new trie node (initialized to NULLs)
-struct TrieNode *getNode(void)
-{
-    struct TrieNode *pNode =  new TrieNode;
-    pNode->isEndOfWord = false;
-    for (int i = 0; i < 26; i++)
-        pNode->children[i] = NULL;
-    return pNode;
-}
-void insert(struct TrieNode *root, string key)
-{
-    struct TrieNode *pCrawl = root;
- 
-    for (int i = 0; i < key.length(); i++)
-    {
-        int index = key[i] - 'a';
-        if (!pCrawl->children[index])
-            pCrawl->children[index] = getNode();
-        pCrawl = pCrawl->children[index];
-    }
-    // mark last node as leaf
-    pCrawl->isEndOfWord = true;
-}    
-struct TrieNode * search(struct TrieNode *root, char key)
-{   if(root==NULL)
-       return root;
-    int index = key - 'a';
-    if (!root->children[index])
-      return NULL;
-    return root->children[index];
-}
-void getThreeMin(vector<string>& v,struct TrieNode *root,vector<char> word){
-    if(root==NULL||v.size()==3)
-        return;
-    if(root->isEndOfWord){
-         v.push_back(string (word.begin(),word.end()));
-    }
-    for(int i=0;i<26;i++){
-        if (root->children[i]){
-            word.push_back((char)(i+'a'));
-            getThreeMin(v,root->children[i],word);
-            word.pop_back();
-        }
-    }
-}    
-public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        struct TrieNode *root = getNode();
-        vector<vector<string>> sol;
-        for (int i = 0; i < products.size(); i++)
-             insert(root, products[i]);
-        vector<char> word;
-        for(int  i =0;i<searchWord.size();i++){
-            root = search(root,searchWord[i]);
-            vector<string> v;
-            word.push_back(searchWord[i]);
-            getThreeMin(v,root,word);
-            sol.push_back(v);
-        }
-        return sol;
-    } 
+    // Node definition of a trie
+    struct Node {
+        bool isWord = false;
+        vector<Node *> children{vector<Node *>(26, NULL)};
+    } * Root, *curr;
 
+    // Runs a DFS on trie starting with given prefix and adds all the words in the result, limiting result size to 3
+    void dfsWithPrefix(Node * curr, string & word, vector<string> & result) {
+        if (result.size() == 3)
+            return;
+        if (curr->isWord)
+            result.push_back(word);
+
+        // Run DFS on all possible paths.
+        for (char c = 'a'; c <= 'z'; c++)
+            if (curr->children[c - 'a']) {
+                word += c;
+                dfsWithPrefix(curr->children[c - 'a'], word, result);
+                word.pop_back();
+            }
+    }
+
+public:
+    Trie() {
+        Root = new Node();
+    }
+    // Inserts the string in trie.
+    void insert(string & s) {
+        // Points curr to the root of trie.
+        curr = Root;
+        for (char &c : s) {
+            if (!curr->children[c - 'a'])
+                curr->children[c - 'a'] = new Node();
+            curr = curr->children[c - 'a'];
+        }
+        // Mark this node as a completed word.
+        curr->isWord = true;
+    }
+    vector<string> getWordsStartingWith(string & prefix) {
+        curr = Root;
+        vector<string> result;
+
+        // Move curr to the end of prefix in its trie representation.
+        for (char &c : prefix) {
+            if (!curr->children[c - 'a'])
+                return result;
+            curr = curr->children[c - 'a'];
+        }
+        dfsWithPrefix(curr, prefix, result);
+        return result;
+    }
+};
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string> &products,
+                                             string searchWord) {
+        Trie trie=Trie();
+        vector<vector<string>> result;
+
+        // Add all words to trie.
+        for(string &w:products)
+            trie.insert(w);
+        string prefix;
+        for (char &c : searchWord) {
+            prefix += c;
+            result.push_back(trie.getWordsStartingWith(prefix));
+        }
+        return result;
+    }
 };
