37bf86d6c35ddbd4824127e3f3fdd114a1e6cae6
fares waheed <faressalm44@hotmail.com>
2022-06-24 18:37:50 +0200

    Time: 68 ms (45.45%), Space: 29.9 MB (61.54%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -4,27 +4,16 @@ public:
         long sum= 0;
         priority_queue<int> q;
         for(auto t:target){
-            if(t!=1){
-                q.push(t);
-            }
+            q.push(t);
             sum+=t;
         }
-        int next=1;
-        while(!q.empty()){
+        while(q.top()!=1){
             int largest = q.top();q.pop();
-            if(sum - largest >= largest || sum - largest == 0)
-                return false;
-            int rest = sum - largest;
-            int frac =(largest/rest);
-            // cout<<"sum: "<<sum<<" frac: "<<frac<<" rest:"<<rest<<" largest: "<<largest<<endl;
-            if(rest==1)
-                return true;
-            largest -=frac * rest;
-            sum -= frac * rest ;
-            if(largest<1||frac==0)
-                return false;
-            if(largest!=1)
-               q.push(largest);
+            sum-=largest;
+            if (largest<= sum || sum < 1) return false;
+            largest%= sum;
+            sum+=largest;
+            q.push(largest?largest: sum);
         }
         return true;
     }
