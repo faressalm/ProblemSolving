b29f833bae37778a4c4ceb8169491d94570c0bc4
fares waheed <faressalm44@hotmail.com>
2022-07-04 07:32:45 +0200

    Time: 29 ms (67.14%), Space: 17.8 MB (65.79%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int low = INT_MAX;
-        int lowIndex=0;
-        vector<pair<int,int>> q;
-        for(int i=0;i<ratings.size();i++)
-          q.push_back({ratings[i],i});
-        sort(q.begin(),q.end());
-        vector<int> candy(ratings.size(),1);
-        int candies =0;
-        for(int i=0;i<q.size();i++){
-            int index = q[i].second;
-            if(index!=0&&ratings[index]>ratings[index-1])
-                candy[index] = 1+ candy[index-1];
-            if(index!=ratings.size()-1&&ratings[index]>ratings[index+1])
-                candy[index] = max(1+candy[index+1],candy[index]);
-            candies +=candy[index]; 
+        vector<int> candies(ratings.size(),1);
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
         }
-        return candies;
+        int sum = candies[ratings.size() - 1];
+        for (int i = ratings.size()- 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+            sum += candies[i];
+        }
+        return sum;
     }
 };
