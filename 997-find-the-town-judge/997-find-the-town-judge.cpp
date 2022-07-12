188e0b51570f8195c8acbaffb58a2945988f196a
fares waheed <faressalm44@hotmail.com>
2022-07-12 17:06:57 +0200

    Time: 258 ms (56.96%), Space: 68.9 MB (33.58%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& nums) {
         vector<int> trusts(n+1,0);
+        vector<bool> shown(n+1,false);
         if(nums.size()<n-1)
             return -1;
-        for(auto trust:nums)
-            trusts[trust[1]]++;
-        int high = trusts[1];
+        int high = 0;
         int index = 1;
-        for(int i=2;i<=n;i++)
-        {
-            if(high<trusts[i])
-            {
-                high= trusts[i];
-                index=i;
+        for(auto trust:nums){
+            shown[trust[0]]=true;
+            trusts[trust[1]]++;
+            if(trusts[trust[1]]>=high){
+                high =trusts[trust[1]];
+                index = trust[1];
             }
         }
         if(high!= n-1)
             return -1;
-        for(auto trust:nums)
-            if(trust[0]==index)
-                return -1;
+        if(shown[index])
+            return -1;
         return index;
     }
 };
