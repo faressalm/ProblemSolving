b67bb8eb19a10abb698c889d9c25c336deaf5446
fares waheed <faressalm44@hotmail.com>
2022-07-17 08:15:17 +0200

    Time: 261 ms (15.53%), Space: 40.1 MB (49.76%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -5,8 +5,8 @@ public:
         for(auto a:arr)
             m[a]=0;
         int rank = 1;
-        for(auto mapEntrey:m)
-            m[mapEntrey.first] = rank++;
+        for(auto &mapEntrey:m)
+            mapEntrey.second = rank++;
         vector<int> sol;
         for(auto a:arr)
             sol.push_back(m[a]);
