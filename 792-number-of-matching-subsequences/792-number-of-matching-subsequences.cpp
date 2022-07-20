7b4dc56886d0fac4d34a4ea21fec6a629bf4f0da
fares waheed <faressalm44@hotmail.com>
2022-07-20 07:25:53 +0200

    Time: 263 ms (73.86%), Space: 49.7 MB (57.16%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int numMatchingSubseq(string s, vector<string>& words) {
-        vector<vector<int>> charMap(26);
-        int n = s.length();
-        
-        for(int i = 0; i < n; i++) {
-            charMap[s[i] - 'a'].push_back(i);
-        }
-        
-        int ans = words.size();
-        
-        for(auto& word : words) {
-            int last = -1;
-            
-            for(char c : word) {
-                auto& cIndexes = charMap[c - 'a'];
-                auto it = upper_bound(cIndexes.begin(), cIndexes.end(), last);
-                if(it == cIndexes.end()) {
-                    ans--;
-                    break;
-                } else {
-                    last = *it;
-                }
+    int numMatchingSubseq(string t, vector<string>& words) {
+        int counter=words.size();
+        vector<vector<int>> hash(26); //chars -> list of indices;
+        for(int i=0; i<t.length();i++) hash[t[i]-'a'].push_back(i);
+        for(auto word:words){
+            int prev = -1;
+            int i;
+            for(auto c:word){
+                vector<int>& vec = hash[c-'a'];
+                auto pos = upper_bound(vec.begin(), vec.end(), prev);
+                // find the first pos whose value is greater than prev
+                if(vec.empty()||pos == vec.end()) {
+                    counter--;
+                    break;}
+                prev = *pos;
             }
         }
-        
-        return ans;
+        return counter;
     }
 };
