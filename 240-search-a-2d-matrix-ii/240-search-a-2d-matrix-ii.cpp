3ea13bfc21a1a83811f4beca49344071affc0682
fares waheed <faressalm44@hotmail.com>
2022-07-24 07:10:38 +0200

    Time: 196 ms (40.63%), Space: 14.8 MB (88.55%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(auto &x:matrix)
-            for(auto &y:x)
-                if(y==target)
-                    return true;
+        int r=0;
+        int c=matrix[0].size()-1;
+        while(r<matrix.size()&&c>=0){
+            if(matrix[r][c]==target)
+                return true;
+            if(matrix[r][c]>target)
+                c--;
+            else r++;
+        }
         return false;
     }
 };
