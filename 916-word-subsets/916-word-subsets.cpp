c7f1e2e8cc0c0f495f21cc5afb93cff682bd6bb4
fares waheed <faressalm44@hotmail.com>
2022-07-30 07:09:34 +0200

    Time: 326 ms (49.22%), Space: 102.4 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        unordered_map<char,int> subset;
+        vector<int> subset(26,0);
         vector<string> sol;
         for(auto word:words2){
-            unordered_map<char,int> temp;
+            vector<int> temp(26,0);
             for(auto &c:word){
-                temp[c]++;
-                subset[c] = max(subset[c],temp[c]);   
+                temp[c-'a']++;
+                subset[c-'a'] = max(subset[c-'a'],temp[c-'a']);   
             }
         }
-        bool take;
         for(auto word:words1){
-            unordered_map<char,int> temp = subset;
+            vector<int> temp = subset;
             for(auto &c:word)
-                if(temp.find(c)!=temp.end())
-                    temp[c]--;
-            take=true;
-            for(auto &m:temp)
-                if(m.second>0)
-                    take=false;
-            if(take)
+                if(temp[c-'a']!=0)
+                    temp[c-'a']--;
+            if(*max_element(temp.begin(),temp.end())<=0)
                 sol.push_back(word);
         }
         return sol;
