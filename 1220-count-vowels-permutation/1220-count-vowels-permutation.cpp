fef9fdfab81e6c756c92a06c850e5124cf25885f
fares waheed <faressalm44@hotmail.com>
2022-08-07 07:12:24 +0200

    Time: 3 ms (95.02%), Space: 5.9 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -30,11 +30,42 @@ private: vector<vector<int>> dp;
     }
 public:
     int countVowelPermutation(int n) {
-        dp.resize(n+1,vector<int>(26,-1));
+        int a=1,b=1,c=1,d=1,e=1;
         int sum=0;
-        for(auto &c:{'a','e','i','o','u'}){
-            sum=(sum+temp(n,c))%mode;
-        }   
-        return sum;
+        int mod=1e9+7;
+        for(int i=0;i<n;i++){
+            sum=(((((a+b)%mode+c)%mode)%mode+d)%mode+e)%mod;
+            
+            int arr[5]={a,b,c,d,e};
+			//counting the occurances which will be ending with respective vowels
+            a=((arr[1]+arr[2])%mode+arr[4])%mod;
+            b=(arr[0]+arr[2])%mod;
+            c=(arr[1]+arr[3])%mod;
+            d=(arr[2])%mod;
+            e=(arr[2]+arr[3])%mod;
+            
+        }
+        return sum%mod;
     }
 };
+
+/**
+    int countVowelPermutation(int n) {
+        long long a=1,b=1,c=1,d=1,e=1;
+        long long sum=0;
+        int mod=1e9+7;
+        for(int i=0;i<n;i++){
+            sum=(a+b+c+d+e)%mod;
+            
+            long long arr[5]={a,b,c,d,e};
+			//counting the occurances which will be ending with respective vowels
+            a=(arr[1]+arr[2]+arr[4])%mod;
+            b=(arr[0]+arr[2])%mod;
+            c=(arr[1]+arr[3])%mod;
+            d=(arr[2])%mod;
+            e=(arr[2]+arr[3])%mod;
+            
+        }
+        return sum%mod;
+    }
+*/
