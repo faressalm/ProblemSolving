e87e35131b0c481afbf213905384d8617ddec3f2
fares waheed <faressalm44@hotmail.com>
2022-08-13 06:59:25 +0200

    Time: 1179 ms (29.76%), Space: 302.3 MB (18.36%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,57 +1,39 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        int wordLen = words[0].size();
-        unordered_map<string,int> orgDic;
-        vector<int> sol;
-        
-        for(auto str: words)
-            orgDic[str]++;
-        int wordsLen =words.size();
-        int window=wordsLen;
-        unordered_map<string,int> dic= orgDic;
-        
-        for(int i= 0;i<s.size();i+=wordLen){
-            string sub = s.substr(i,wordLen);
-            int startInd =i-(wordsLen-window)*wordLen;
-            if(dic.count(sub)==0){
-                i=startInd-wordLen+1;
-                window=wordsLen;
-                dic=orgDic;
-            }
-            else if(dic[sub]!=0){
-                dic[sub]--;
-                window--;
-                if(window==0){
-                    sol.push_back(startInd);
-                    if(dic.count(s.substr(startInd+1,wordLen))!=0){
-                      i=startInd+1-wordLen;
-                      window=wordsLen;
-                      dic=orgDic;
-                    }else{
-                    window++;
-                    dic[s.substr(startInd,wordLen)]++;
-                    }
-
-                }
-            }else{
-
-                     string startStr = s.substr(startInd,wordLen);
-                if(dic.count(s.substr(startInd+1,wordLen))!=0){
-                      i=startInd+1-wordLen;
-                      window=wordsLen;
-                      dic=orgDic;
-                    }else{
-                                    while(startStr!=sub){
-                 window++;
-                 dic[startStr]++;  
-                 startInd+=wordLen;   
-                 startStr = s.substr(startInd,wordLen);   
-                }
-                }
-
-            }
+        vector<int>ans;
+        int n=words.size();
+        int m=words[0].size();
+        int total=n*m;
+        if(s.size()<total){
+            return ans;
         }
-        return sol;
+        for(int i=0;i<=s.size()-total;i++){
+            unordered_map<string,int>mp;
+            
+            for(int j=0;j<words.size();j++){
+                mp[words[j]]++;
+            }
+            int k;
+            for(k=0;k<n;k++){
+                string temp=s.substr(i+k*m,m);
+                if(mp.count(temp)==0){
+                    break;
+                }
+                else{
+                    if(mp[temp]!=0){
+                        mp[temp]--;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+            if(k==n){
+                ans.push_back(i);
+            }
+            
+        }
+        return ans ;
     }
 };
