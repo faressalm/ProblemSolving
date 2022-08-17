b70ffaab0168ced58701cbafafcb168bae6e1c01
fares waheed <faressalm44@hotmail.com>
2022-08-17 06:42:40 +0200

    Time: 0 ms (100.00%), Space: 8.6 MB (61.19%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> alph = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_set<string> s;
+        string morse="";
+        for(auto word:words){
+            morse="";
+            for(auto letter:word){
+                morse+=alph[letter-'a'];
+            }
+            s.insert(morse);
+        }
+        return s.size();
+    }
+};
