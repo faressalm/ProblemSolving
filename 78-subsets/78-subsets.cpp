acfc226a45b8db4dc7cbf45a80a426c01d63ff82
fares waheed <faressalm44@hotmail.com>
2022-08-17 06:52:59 +0200

    Time: 7 ms (36.24%), Space: 7.1 MB (87.56%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> s={{}};
+        int size;
+        for(auto num:nums){
+            size = s.size();
+            for(int i=0;i<size;i++){
+                vector<int> newv(s[i]);
+                newv.push_back(num);
+                s.push_back(newv);
+            }
+        }
+        return s;
+    }
+};
