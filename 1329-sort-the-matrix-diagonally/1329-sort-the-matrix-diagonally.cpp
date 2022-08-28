58a3850a08c40101294434c13a972642516e365c
fares waheed <faressalm44@hotmail.com>
2022-08-28 07:25:01 +0200

    Time: 16 ms (64.04%), Space: 9.4 MB (28.08%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,31 +1,24 @@
 class Solution {
+private: void pos(vector<vector<int>>& mat,int r,int c,int row,int col){
+            int rr=r;int cc=c;
+            vector<int> temp;
+            while(r<row&&c<col){
+                temp.push_back(mat[r][c]);
+                r++;c++;
+            }
+            sort(temp.begin(),temp.end());
+            r=rr;c=cc;
+            for(int t=0;t<temp.size();t++)
+                mat[r+t][c+t]=temp[t];
+}
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         int row =mat.size();int col = mat[0].size();
-        
         for(int i=0;i<col;i++){
-            int r=0;int c =i;
-            vector<int> temp;
-            while(r<row&&c<col){
-                temp.push_back(mat[r][c]);
-                r++;c++;
-            }
-            sort(temp.begin(),temp.end());
-            r=0;c=i;
-            for(int t=0;t<temp.size();t++)
-                mat[r+t][c+t]=temp[t];
+            pos(mat,0,i,row,col);
         }
         for(int i=1;i<row;i++){
-            int r=i;int c=0;
-            vector<int> temp;
-            while(r<row&&c<col){
-                temp.push_back(mat[r][c]);
-                r++;c++;
-            }
-            sort(temp.begin(),temp.end());
-            r=i;c=0;
-            for(int t=0;t<temp.size();t++)
-                mat[r+t][c+t]=temp[t];
+            pos(mat,i,0,row,col);
         }
         return mat;
     }
