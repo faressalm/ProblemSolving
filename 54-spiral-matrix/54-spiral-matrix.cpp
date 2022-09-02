af2f4db42f0b54a3a6116677f304c06c0d1b5f2f
fares waheed <faressalm44@hotmail.com>
2022-09-02 15:25:49 +0200

    Time: 5 ms (15.78%), Space: 7 MB (29.76%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -2,11 +2,14 @@ class Solution {
 private: void temp(int r1,int r2,int c1, int c2, vector<int> &spiral,vector<vector<int>>& matrix){
     for(int c=c1;c<c2;c++)
         spiral.push_back(matrix[r1][c]);
-    for(int r=r1+1;r<r2&&(c1<c2);r++)
+    bool e = (c1<c2);
+    for(int r=r1+1;r<r2&&e;r++)
         spiral.push_back(matrix[r][c2-1]);
-    for(int c=c2-2;c>=c1&&(r1+1<r2)&&(c1<c2);c--)
+    e= e&&(r1+1<r2);
+    for(int c=c2-2;c>=c1&&e;c--)
         spiral.push_back(matrix[r2-1][c]);
-    for(int r=r2-2;r>r1&&(c2-2>=c1)&&(r1+1<r2)&&(c1<c2);r--)
+    e =e&&(c2-2>=c1);
+    for(int r=r2-2;r>r1&&e;r--)
         spiral.push_back(matrix[r][c1]);
 }
 public:
