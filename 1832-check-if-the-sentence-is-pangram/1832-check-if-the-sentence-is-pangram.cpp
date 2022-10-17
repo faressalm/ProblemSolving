7e1110a92bc092d64aed28c788482006949b5dcc
fares waheed <faressalm44@hotmail.com>
2022-10-17 06:51:23 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (98.63%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        if(sentence.size()<26)
-            return false;
         int letters =0;
         for(auto &c:sentence)
             letters |= 1<< (c-'a');
